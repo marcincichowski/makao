e566a837cec91780681c11486c7fb1124142e57c
MarcinCichowski <marcin.cichowski@student.put.poznan.pl>
2020-05-30 14:42:22 +0200

    Handling window events

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "headers/Cards.h"
 #include "headers/Player.h"
 #include "headers/Deck.h"
@@ -6,5 +7,15 @@
 
 int main(){
 
+    sf::RenderWindow window(sf::VideoMode(800,600),"Makao");
+    while(window.isOpen()){
+
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed){
+               window.close();
+            }
+        }
+    }
     return 0;
 }
